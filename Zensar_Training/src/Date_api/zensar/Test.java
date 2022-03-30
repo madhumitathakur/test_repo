aaf9fe65b9ad4b3fb6abbbe6792d732afc640a0b
Anand Kulkarni <anand.kulkarni1@zensar.com>
2022-03-30 16:44:56 +0530

    first commit

--- Zensar_Training/src/Date_api/zensar/Test.java
+++ Zensar_Training/src/Date_api/zensar/Test.java
@@ -3,6 +3,7 @@ package Date_api.zensar;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		int [][]arr = {{1,0,0,0}, {0,1,1,0}, {1,1,1,1}};
 		for (int[] x : arr) {
 			for(int data: x) {
